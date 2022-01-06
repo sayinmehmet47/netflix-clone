5638f09ecbbf6adc86f149ddc6cbe12119aed74f
unknown <sayinmehmet47@gmail.com>
2022-01-06 13:54:47 +0200

    added corousel to browser page

--- pages/browse.tsx
+++ pages/browse.tsx
@@ -6,8 +6,16 @@ import { IoAlertCircleOutline } from "react-icons/io5";
 import { BsSearch } from "react-icons/bs";
 import { BsBell } from "react-icons/bs";
 import CarouselComponent from "./components/Carousel";
-import fetchRequest from "./fetchRequest";
 import DropdownUser from "./components/dropdownUser";
+const fetchRequest = {
+  netflixOriginalsFetch: `/discover/tv?api_key=32d0c559d4f922d14ea1f7f066e100a4&with_networks=213`,
+  trendingFetch: `/trending/all/week?api_key=32d0c559d4f922d14ea1f7f066e100a4&language=en-US`,
+  topRatedFetch: `/movie/top_rated?api_key=32d0c559d4f922d14ea1f7f066e100a4&language=en-US`,
+  comedyMovieFetch: `/discover/movie?api_key=32d0c559d4f922d14ea1f7f066e100a4&with_genres=35`,
+  actionMovieFetch: `/discover/movie?api_key=32d0c559d4f922d14ea1f7f066e100a4&with_genres=28`,
+  horrorMovieFetch: `/discover/movie?api_key=32d0c559d4f922d14ea1f7f066e100a4&with_genres=27`,
+  romanceMovieFetch: `/discover/movie?api_key=32d0c559d4f922d14ea1f7f066e100a4&with_genres=10749`,
+};
 export default function Browse({ movies }) {
   console.log(movies);
   const { poster_path, original_title, overview } = movies;
