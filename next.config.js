a8797b9a7184dd4ee9cc897cd937e80027785245
unknown <sayinmehmet47@gmail.com>
2022-01-07 17:02:21 +0200

    avatar domain added

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
       "occ-0-4451-778.1.nflxso.net",
       "www.themoviedb.org",
       "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
     ],
   },
   async redirects() {
