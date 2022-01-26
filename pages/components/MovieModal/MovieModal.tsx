bc24fe680633ccc7707a77a9910d0fbbe25cf6a8
unknown <sayinmehmet47@gmail.com>
2022-01-26 10:22:28 +0200

    modal added

--- pages/components/MovieModal/MovieModal.tsx
+++ pages/components/MovieModal/MovieModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./MovieModal.module.css";
 import Image from "next/image";
 type Props = {
@@ -18,25 +18,33 @@ export default function MovieModal({
   setModalVisibility,
   movieSelected,
 }: Props) {
-  const path = `https://www.themoviedb.org/t/p/w1280_and_h720_multi_faces/${movieSelected?.backdrop_path}`;
+  const [path, setPath] = useState("");
 
-  console.log(movieSelected.backdrop_path);
+  useEffect(() => {
+    setPath(
+      `https://www.themoviedb.org/t/p/w1280_and_h720_multi_faces/${movieSelected?.backdrop_path}`
+    );
+  }, [movieSelected]);
+
+  console.log(path);
 
   return (
     <div
       id={styles.noScrollbar}
       className="fixed overflow-y-scroll h-96 z-40  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md bg-zinc-800"
     >
-      <Image
-        id={styles.lightboxImg}
-        width={1280}
-        height={720}
-        alt="fd"
-        src={path}
-      />
+      {path && (
+        <Image
+          id={styles.lightboxImg}
+          width={1280}
+          height={720}
+          alt="fd"
+          src={path}
+        />
+      )}
 
-      <h1>fdsfkl</h1>
-      <h1>fdsfkl</h1>
+      <h1>{path}</h1>
+      <button onClick={() => setModalVisibility(false)}>close</button>
 
       <h1>fdsfkl</h1>
 
