3d626af2646da39b4aaa815c98e9c57bd2cc4cab
mehmet <sayinmehmet47@gmail.com>
2023-09-09 23:53:05 +0200

    feat: google verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
-import Header from "./components/header";
-import { useSession } from "next-auth/react";
-import prisma from "../lib/prisma";
-import { GetServerSideProps } from "next";
-import Navbar from "./components/navbar";
+import Header from './components/header';
+import { useSession } from 'next-auth/react';
+import prisma from '../lib/prisma';
+import { GetServerSideProps } from 'next';
+import Navbar from './components/navbar';
+import Head from 'next/head';
 
 export default function Home({ feed }) {
   const { data } = useSession();
 
   return (
     <div className="bg-netflix   h-screen bg-cover bg-center">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="tsIPr7tuwtFUuym9hEuBkGYAe7oZzebwImKTiSXFH0Q"
+        />
+      </Head>
       <div className="h-screen bg-black/60">
         {/* <Header /> */}
         <Navbar />
